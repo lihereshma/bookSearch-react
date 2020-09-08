277ee599e3060fc1f07439aeb12f31e0a44938a2
lihereshma <reshmalihe00068@gmail.com>
2020-09-08 16:44:13 +0530

    Updates

---
+++ precache-manifest.c797923199728ba793f9856bdd590636.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef651389bda8b2543c1e9657436ca0f8",
+    "url": "/bookSearch-react/index.html"
+  },
+  {
+    "revision": "d37fd36b074c28ed4726",
+    "url": "/bookSearch-react/static/css/main.8e84ceb4.chunk.css"
+  },
+  {
+    "revision": "605a473176f2662962d6",
+    "url": "/bookSearch-react/static/js/2.7da56a84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bookSearch-react/static/js/2.7da56a84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d37fd36b074c28ed4726",
+    "url": "/bookSearch-react/static/js/main.7bec49c6.chunk.js"
+  },
+  {
+    "revision": "580193e5faa09d8f2175",
+    "url": "/bookSearch-react/static/js/runtime-main.d1a8a311.js"
+  }
+]);
